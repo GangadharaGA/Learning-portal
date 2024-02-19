14841ba7f0eeb30ce9332f34660fa82ab15318ba
GangadharaGA <94308264+GangadharaGA@users.noreply.github.com>
2024-02-19 18:08:48 +0530

    Update UserRepository.java
    Added FindByUsername

--- learning-portal/src/main/java/com/gangadhara/learningportalnew/repository/UserRepository.java
+++ learning-portal/src/main/java/com/gangadhara/learningportalnew/repository/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.gangadhara.learningportalnew.entity.UserEntity;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
-
+        UserEntity findByusername(String name);
 }
