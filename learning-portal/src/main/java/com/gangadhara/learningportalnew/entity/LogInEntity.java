1f592a6a378f7b46c0be721f224316b391cf0d5a
GangadharaGA <94308264+GangadharaGA@users.noreply.github.com>
2024-02-19 18:06:33 +0530

    Added log_in
    New Login page using username and password

---
+++ learning-portal/src/main/java/com/gangadhara/learningportalnew/entity/LogInEntity.java
@@ -0,0 +1,15 @@
+package com.gangadhara.learningportalnew.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class LogInEntity {
+	
+	private String  username;
+	private String password;
+
+}
